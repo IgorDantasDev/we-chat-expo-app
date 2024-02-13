fa6e42cd2f59d32c09ad6e4d20006056dda73ee7
IbraDajani <ibrahimdajani121@gmail.com>
2024-02-12 21:29:07 -0300

    fixing enhancer error

--- App.tsx
+++ App.tsx
@@ -7,12 +7,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { HEADER_COLOR } from "~/constants/colors";
 import { store } from "~/store";
+import { LogBox } from "react-native";
 
 if (__DEV__) {
   // @ts-ignore
   import("~/config/reactotronConfig");
 }
 const App = () => {
+
+  LogBox.ignoreLogs(['middlewares were provided, but middleware enhancer was not included in final enhancers - make sure to call `getDefaultEnhancers'])
+
+
   return (
     <Provider store={store}>
       <NavigationContainer>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
+import {configureStore} from '@reduxjs/toolkit';
+import {persistStore} from 'redux-persist';
 
-import { persistedReducers } from "./persist";
+import {persistedReducers} from './persist';
 // @ts-ignore
-import Reactotron from "../config/reactotronConfig";
+import Reactotron from '../config/reactotronConfig';
+import {buildGetDefaultEnhancers} from '@reduxjs/toolkit/dist/getDefaultEnhancers';
 
 export const store = configureStore({
   reducer: persistedReducers(),
   // @ts-ignore
-  enhancers: Reactotron ? [Reactotron.createEnhancer()] : undefined,
-  middleware: (getDefaultMiddleware) =>
+  enhancers: () => (Reactotron ? [Reactotron.createEnhancer()] : undefined),
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
